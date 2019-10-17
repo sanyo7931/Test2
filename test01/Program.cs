895dc57d026d42f214b22c2db93d70c250fab308
sanyo7931 <sanyo7931@yahoo.com.tw>
2019-10-17 20:35:46 +0800

    測試2

---
+++ test01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test01
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            Random random = new Random(DateTime.Now.Millisecond);
+            for(int i=0;i<10;i++)
+            { 
+                int randomNum = random.Next(10);
+                Console.WriteLine(randomNum);
+            }
+            Console.WriteLine("Stop");
+            Console.ReadKey();
+        }
+    }
+}
